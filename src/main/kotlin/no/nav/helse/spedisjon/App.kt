7c75b0acd4ac2dc0198db9a72fa626f00296da46
David Steinsland <david@steinsland.email>
2020-03-15 21:57:36 +0100

    fortsett fra committed offset

--- src/main/kotlin/no/nav/helse/spedisjon/App.kt
+++ src/main/kotlin/no/nav/helse/spedisjon/App.kt
@@ -8,7 +8,7 @@ fun main() {
     val dataSourceBuilder = DataSourceBuilder(env)
     val meldingDao = MeldingDao(dataSourceBuilder.getDataSource())
 
-    RapidApplication.create(env){ _, rapid -> rapid.seekToBeginning() }.apply {
+    RapidApplication.create(env).apply {
         NyeSøknader(this, meldingDao)
         SendteSøknader(this, meldingDao)
         Inntektsmeldinger(this, meldingDao)
