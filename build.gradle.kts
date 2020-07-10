55421b69e329de77774e625b2a1eaa82cf966d69
Martin Solheim <martin.solheim@me.com>
2020-07-10 12:38:07 +0200

    Bumper depemdencies og gradle versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val junitJupiterVersion = "5.6.0"
-val mainClass = "no.nav.helse.spedisjon.AppKt"
+val junitJupiterVersion = "5.6.2"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -13,20 +10,23 @@ buildscript {
     }
 }
 
-val githubUser: String by project
-val githubPassword: String by project
+repositories {
+    mavenCentral()
+    maven("https://kotlin.bintray.com/ktor")
+    maven("https://jitpack.io")
+}
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.74ae9cb")
+    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
 
-    implementation("org.flywaydb:flyway-core:6.3.1")
-    implementation("com.zaxxer:HikariCP:3.4.2")
-    implementation("no.nav:vault-jdbc:1.3.1")
+    implementation("org.flywaydb:flyway-core:6.5.0")
+    implementation("com.zaxxer:HikariCP:3.4.5")
+    implementation("no.nav:vault-jdbc:1.3.7")
     implementation("com.github.seratch:kotliquery:1.3.1")
 
     testImplementation("com.opentable.components:otj-pg-embedded:0.13.3")
-    testImplementation("io.mockk:mockk:1.9.3")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.26.3") {
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.27.1") {
         exclude(group = "junit")
     }
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
@@ -34,57 +34,37 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
-repositories {
-    mavenCentral()
-    maven("https://kotlin.bintray.com/ktor")
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/rapids-and-rivers")
-        credentials {
-            username = githubUser
-            password = githubPassword
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "12"
+    }
+
+    named<Jar>("jar") {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.spedisjon.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
         }
     }
-}
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_12
-    targetCompatibility = JavaVersion.VERSION_12
-}
-
-tasks.named<Jar>("jar") {
-    archiveBaseName.set("app")
-
-    manifest {
-        attributes["Main-Class"] = mainClass
-        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
-            it.name
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
         }
     }
-
-    doLast {
-        configurations.runtimeClasspath.get().forEach {
-            val file = File("$buildDir/libs/${it.name}")
-            if (!file.exists())
-                it.copyTo(file)
-        }
-    }
-}
-
-tasks.named<KotlinCompile>("compileKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "6.1"
 }
