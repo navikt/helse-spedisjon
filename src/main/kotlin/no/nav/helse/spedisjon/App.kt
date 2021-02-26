d2a0b3731ed208986fd386b8290625e8059236be
David Steinsland <david@steinsland.email>
2021-02-26 12:35:25 +0100

    produserer til aiven dersom aiven topic er satt
    Co-authored-by: Aleksander Andresen <aleksander.andresen@nav.no>

--- src/main/kotlin/no/nav/helse/spedisjon/App.kt
+++ src/main/kotlin/no/nav/helse/spedisjon/App.kt
@@ -4,7 +4,16 @@ import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.spedisjon.AktørregisteretClient.AktørregisteretRestClient
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.security.auth.SecurityProtocol
+import org.apache.kafka.common.serialization.StringSerializer
 import java.io.File
+import java.util.*
 
 fun main() {
     val env = System.getenv()
@@ -17,9 +26,11 @@ fun main() {
         password = "/var/run/secrets/nais.io/service_user/password".readFile()
     ))
 
+    val aivenProducer = createAivenProducer(env)
+
     val meldingMediator = MeldingMediator(meldingDao, aktørregisteretClient, env["STREAM_TO_RAPID"]?.let { "false" != it.toLowerCase() } ?: true)
 
-    LogWrapper(RapidApplication.create(env), meldingMediator).apply {
+    LogWrapper(RapidApplication.create(env), meldingMediator, env["KAFKA_RAPID_TOPIC_AIVEN"], aivenProducer).apply {
         NyeSøknader(this, meldingMediator)
         SendteSøknaderArbeidsgiver(this, meldingMediator)
         SendteSøknaderNav(this, meldingMediator)
@@ -33,9 +44,31 @@ fun main() {
     }.start()
 }
 
+private fun createAivenProducer(env: Map<String, String>): KafkaProducer<String, String>? {
+    if (!env.containsKey("KAFKA_RAPID_TOPIC_AIVEN")) return null
+    val properties = Properties().apply {
+        put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, env.getValue("KAFKA_BROKERS"))
+        put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
+        put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
+        put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, env.getValue("KAFKA_KEYSTORE_PATH"))
+        put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+        put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, env.getValue("KAFKA_TRUSTSTORE_PATH"))
+        put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, env.getValue("KAFKA_CREDSTORE_PASSWORD"))
+        put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, env.getValue("KAFKA_KEYSTORE_PATH"))
+        put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, env.getValue("KAFKA_CREDSTORE_PASSWORD"))
+
+        put(ProducerConfig.ACKS_CONFIG, "1")
+        put(ProducerConfig.LINGER_MS_CONFIG, "0")
+        put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1")
+    }
+    return KafkaProducer(properties, StringSerializer(), StringSerializer())
+}
+
 internal class LogWrapper(
     private val rapidsConnection: RapidsConnection,
-    private val meldingMediator: MeldingMediator
+    private val meldingMediator: MeldingMediator,
+    private val aivenTopic: String? = null,
+    private val aivenProducer: KafkaProducer<String, String>? = null
 ) : RapidsConnection(), RapidsConnection.MessageListener {
 
     init {
@@ -49,11 +82,11 @@ internal class LogWrapper(
     }
 
     override fun publish(message: String) {
-        rapidsConnection.publish(message)
+        aivenProducer?.send(ProducerRecord(aivenTopic, message)) ?: rapidsConnection.publish(message)
     }
 
     override fun publish(key: String, message: String) {
-        rapidsConnection.publish(key, message)
+        aivenProducer?.send(ProducerRecord(aivenTopic, key, message)) ?: rapidsConnection.publish(key, message)
     }
 
     override fun start() {
