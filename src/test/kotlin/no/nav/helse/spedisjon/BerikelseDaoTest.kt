a085189a56dabce0e69a13ff8dad4392a2932b27
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-08-03 09:21:45 +0200

    Legger til et test på at vi markerer som besvart

--- src/test/kotlin/no/nav/helse/spedisjon/BerikelseDaoTest.kt
+++ src/test/kotlin/no/nav/helse/spedisjon/BerikelseDaoTest.kt
@@ -2,9 +2,11 @@ package no.nav.helse.spedisjon
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.rapids_rivers.RapidsConnection
-import org.junit.jupiter.api.Assertions.assertEquals
+import no.nav.helse.spedisjon.Melding.Companion.sha512
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.util.UUID
 import javax.sql.DataSource
 
 internal class BerikelseDaoTest: AbstractRiverTest() {
@@ -32,10 +34,20 @@ internal class BerikelseDaoTest: AbstractRiverTest() {
     }
 
     @Test
-    fun `ubsevart behov inneholder nødvendig informasjon`() {
+    fun `ubesvart behov inneholder nødvendig informasjon`() {
         val dao = BerikelseDao(dataSource)
         dao.behovEtterspurt("fnr", "duplikatkontroll", listOf("aktørId", "fødselsdato"), LocalDateTime.now().minusMinutes(1))
         assertEquals(listOf(UbesvartBehov("fnr", "duplikatkontroll".padEnd(128, ' '), listOf("aktørId", "fødselsdato"))), dao.ubesvarteBehov(LocalDateTime.now()))
+    }
+
+    @Test
+    fun `markeres som besvart når vi mottar løsning`() {
+        val duplkatkontroll = "${UUID.randomUUID()}".sha512()
+        val dao = BerikelseDao(dataSource)
+        dao.behovEtterspurt("fnr", duplkatkontroll, listOf("aktørId", "fødselsdato"), LocalDateTime.now())
+        assertFalse(dao.behovErBesvart(duplkatkontroll))
+        dao.behovBesvart(duplkatkontroll, jacksonObjectMapper().createObjectNode())
+        assertTrue(dao.behovErBesvart(duplkatkontroll))
     }
 
     override fun createRiver(rapidsConnection: RapidsConnection, dataSource: DataSource) {}
