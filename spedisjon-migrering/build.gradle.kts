b9a4af67f576e4ecc2feaff02904b114ea0826a9
david steinsland <david@davidsteinsland.net>
2024-11-19 09:50:29 +0100

    legger til manglende fil

---
+++ spedisjon-migrering/build.gradle.kts
@@ -0,0 +1,58 @@
+val mainClass = "no.nav.helse.spedisjon.migrering.AppKt"
+
+val logbackClassicVersion = "1.5.12"
+val logbackEncoderVersion = "8.0"
+val flywayVersion = "10.21.0"
+val cloudSqlVersion = "1.21.0"
+val postgresqlVersion = "42.7.4"
+val hikariCPVersion = "6.1.0"
+val kotliqueryVersion = "1.9.0"
+val jacksonVersion = "2.18.1"
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:$logbackClassicVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion") {
+        exclude("com.fasterxml.jackson.core")
+        exclude("com.fasterxml.jackson.dataformat")
+    }
+
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+    implementation("com.google.cloud.sql:postgres-socket-factory:$cloudSqlVersion")
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
+    implementation("com.zaxxer:HikariCP:$hikariCPVersion")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+}
+
+tasks {
+    val copyJars = create("copy-jars") {
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("${layout.buildDirectory.get()}/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
+        }
+    }
+    get("build").finalizedBy(copyJars)
+
+    withType<Jar> {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = mainClass
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+    }
+}
