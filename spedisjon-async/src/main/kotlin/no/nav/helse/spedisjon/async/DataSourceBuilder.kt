3afcbe3b50c22b59dfa52767f2b9a9de2ebdd7d4
david steinsland <david@davidsteinsland.net>
2024-11-21 15:57:12 +0100

    nais-plattformen setter nå en brukandes jdbc-url i env
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- spedisjon-async/src/main/kotlin/no/nav/helse/spedisjon/async/DataSourceBuilder.kt
+++ spedisjon-async/src/main/kotlin/no/nav/helse/spedisjon/async/DataSourceBuilder.kt
@@ -8,9 +8,7 @@ import org.slf4j.LoggerFactory
 internal class DataSourceBuilder(env: Map<String, String>) {
 
     private val baseConnectionConfig = HikariConfig().apply {
-        jdbcUrl = cloudSqlProxyConnectionString(env.getValue("DATABASE_HOST"), env.getValue("DATABASE_PORT").toInt(), env.getValue("DATABASE_DATABASE"))
-        username = env.getValue("DATABASE_USERNAME")
-        password = env.getValue("DATABASE_PASSWORD")
+        jdbcUrl = env.getValue("DATABASE_JDBC_URL")
     }
 
     private val migrationConfig = HikariConfig().apply {
@@ -38,9 +36,5 @@ internal class DataSourceBuilder(env: Map<String, String>) {
 
     private companion object {
         private val logger = LoggerFactory.getLogger(DataSourceBuilder::class.java)
-
-        private fun cloudSqlProxyConnectionString(databaseHost: String, databasePort: Int, databaseName: String): String {
-            return String.format("jdbc:postgresql://%s:%d/%s", databaseHost, databasePort, databaseName)
-        }
     }
 }
