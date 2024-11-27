c87810369582239445716b62c6992b718995f03b
david steinsland <david@davidsteinsland.net>
2024-11-27 16:39:42 +0100

    tester daoen

---
+++ spedisjon-async/src/test/kotlin/no/nav/helse/spedisjon/async/EkspederingDaoTest.kt
@@ -0,0 +1,21 @@
+package no.nav.helse.spedisjon.async
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class EkspederingDaoTest {
+
+    @Test
+    fun `duplikat melding`() {
+        val testDataSource = databaseContainer.nyTilkobling()
+        try {
+            val dao = EkspederingDao(testDataSource.ds)
+            val id = UUID.randomUUID()
+            assertTrue(dao.meldingEkspedert(id))
+            assertFalse(dao.meldingEkspedert(id))
+        } finally {
+            databaseContainer.droppTilkobling(testDataSource)
+        }
+    }
+}
