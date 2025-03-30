6a3884d8adfc06d0ebfe8115630a990786aadd3d
david steinsland <david@davidsteinsland.net>
2025-03-30 13:23:17 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2025032719521743101565.4abbf0c3af65"
-val tbdLibsVersion = "2025.03.27-18.30-c228796d"
+val rapidsAndRiversVersion = "2025033013081743332933.20de2f3d8983"
+val tbdLibsVersion = "2025.03.30-13.02-f7cb11ef"
 
 plugins {
     base
