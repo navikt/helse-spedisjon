562fb4f42b68100b4ddae2ca1b36847e21ba0585
Sivert Lunsæter <sfl@computas.com>
2025-08-06 14:16:53 +0200

    Fiks typo i forbid valie i AndreSøknaderRiver
    Co-authored-by: Hakon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Vemund Strøm <vemund.strom@nav.no>

--- spedisjon-async/src/main/kotlin/no/nav/helse/spedisjon/async/AndreSøknaderRiver.kt
+++ spedisjon-async/src/main/kotlin/no/nav/helse/spedisjon/async/AndreSøknaderRiver.kt
@@ -23,7 +23,7 @@ internal class AndreSøknaderRiver(
                 it.forbidValue("type", "ARBEIDSLEDIG")
                 if (System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
                     // vi støtter selvstendig og arbeidsledig i dev, men bare for ordinære Selvstendig næringsdrivende (ikke Jordbruker etc)
-                    it.forbidValue("arbeidssituasjon", "SELVSTENDIG_NÆRINGSDRIVENDE")
+                    it.forbidValue("arbeidssituasjon", "SELVSTENDIG_NARINGSDRIVENDE")
                 }
             }
             validate {
